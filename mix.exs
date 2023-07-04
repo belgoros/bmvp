04ff952f2eab61db19a2d8cec27545763995fce0
belgoros <s.cambour@gmail.com>
2023-07-04 12:11:20 +0200

    Add faker dependency

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Bmvp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:faker, "~> 0.17.0"}
     ]
   end
 
