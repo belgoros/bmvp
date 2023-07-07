96aa97450505beed3e2d82526f12cf8489b2f444
belgoros <s.cambour@gmail.com>
2023-07-07 16:20:05 +0200

    Implement webhook_handler

---
+++ lib/bmvp_web/controllers/webhook_handler.ex
@@ -0,0 +1,25 @@
+defmodule BmvpWeb.WebhookHandler do
+  @behaviour LemonEx.Webhooks.Handler
+
+  alias Bmvp.Accounts.UserNotifier
+  alias Bmvp.Articles
+  alias BmvpWeb.UrlHelper
+
+  @impl true
+  def handle_event(%LemonEx.Webhooks.Event{name: "order_created"} = event) do
+    article_id = event.meta["custom_data"]["article_id"]
+    user_email = email.data.user_email
+
+    with {:ok, article} <- Articles.get_article(article_id),
+         url <- UrlHelper.gen_unique_article_url(article_id),
+         {:ok, _email} <- UserNotifier.deliver_article_url(user_email, url, article) do
+      :ok
+    end
+  end
+
+  # You need to handle all incoming events. So, better have a
+  # catch-all handler for events that you don't want to handle,
+  # but only want to acknowledge.
+  @impl true
+  def handle_event(_unhandled_event), do: :ok
+end
--- lib/bmvp_web/endpoint.ex
+++ lib/bmvp_web/endpoint.ex
@@ -11,41 +11,51 @@ defmodule BmvpWeb.Endpoint do
     same_site: "Lax"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :bmvp,
     gzip: false,
     only: BmvpWeb.static_paths()
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :bmvp
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :bmvp)
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(LemonEx.Webhooks.Plug,
+    # <- At which path the Plug expects to receive webhooks
+    at: "/webhook/lemonsqueezy",
+    # <- Your handler module
+    handler: BmvpWeb.WebhookHandler
+  )
+
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug BmvpWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(BmvpWeb.Router)
 end
